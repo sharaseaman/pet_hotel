452534c54ff2aeed79c5dcce93f1f4fff767bb74
Alex Hand <ahand@Alexs-MacBook-Pro.local>
2017-09-21 16:44:09 -0500

    working on delete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,10 +25,21 @@ myApp.controller('WardenController', function ($http) {
         $http({
             method: 'POST',
             url: '/pet',
-            data: {name: vm.nameIn, breed: vm.breedIn}
+            data: {name: vm.nameIn, breed: vm.breedIn, color: vm.colorIn, checked: vm.checkedIn}
             }).then(function(response){
                 vm.getPets(); 
                 console.log('response', response);
         }) //and then done
     }; //end addPet function
+
+    vm.deletePet = function() {
+        var theThing = vm.pet.id;
+        console.log('In deletePet with vm ->', theThing);
+
+        // $http({
+        //     method: 'DELETE',
+        //     url: '/pet',
+        //     data: 
+        // })
+    }
 }); //end controller function
